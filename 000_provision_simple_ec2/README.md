e1043866312e565e5f2535ae53e999c38e4e364f
chriss <chriss@chriss>
2022-09-12 09:05:52 +0200

    Added variable instance type and output publi IP address

--- 000_provision_simple_ec2/README.md
+++ 000_provision_simple_ec2/README.md
@@ -2,3 +2,5 @@
 
 The script `main.tf` shows how to provision a simple EC2 instance. You can select a different instance type `instance_type` and a different image `ami`.
 Run `terraform init` and then `terraform apply` to provision the instance. `terraform destroy` terminates the instance.
+
+The EC2 instance type can be configured via command line invocation, e.g. `terraform plan -var=instance_type="t2.nano"`.
--- 000_provision_simple_ec2/main.tf
+++ 000_provision_simple_ec2/main.tf
@@ -11,11 +11,24 @@ provider "aws" {
   profile = "default"
 }
 
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+locals {
+  project_suffix = "test"
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.my_ec2.public_ip
+}
+
 resource "aws_instance" "my_ec2" {
   ami           = "ami-05fa00d4c63e32376"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "ExampleEC2Instance"
+    Name = "EC2_${local.project_suffix}"
   }
 }
